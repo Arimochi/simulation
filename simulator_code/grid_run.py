8570d58299bc92b67c9c9e5ab17b21c04f9f117f
Arimochi <112501001+Arimochi@users.noreply.github.com>
2022-12-27 00:52:23 +0900

    Add files via upload

--- simulator_code/grid_run.py
+++ simulator_code/grid_run.py
@@ -20,12 +20,13 @@ for num in range(times):
     print(str(num + 1) + "回目")
     file_list.append(str(num + 1) + infile)
     a = np.random.randint(12345,123456)
-    if a not in ns:
-        ns.append(a)
-        os.system("python grid_simulator.py " + str(a))
-    else:
-        a = np.random.randint(12345,123456)
-        os.system("python grid_simulator.py " + str(a))
+    while(True):
+        if a not in ns:
+            ns.append(a)
+            os.system("python grid_simulator.py " + str(a))
+            break
+        else:
+            a = np.random.randint(12345,123456)
     #os.system("python argv.py " + str(a))
     #file_dic[num] = "result grid5x5.net.xml rate=1.0cars100obstacles10fake_cars0fake_obs0.csv"
 
