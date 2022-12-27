9a57531dadedd77f75f00f0975ae8a879c025e39
Arimochi <112501001+Arimochi@users.noreply.github.com>
2022-12-27 23:28:18 +0900

    Add files via upload

--- simulator_code/wilcoxon_anlaysis.py
+++ simulator_code/wilcoxon_anlaysis.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # coding: utf-8
 import numpy as np
+import os
 
 ### read data ###
 #infilename1 = "result grid3x3.net.xml rate=1.0cars100obstacles10fake_cars0fake_obs0.csv"
@@ -10,9 +11,17 @@ import numpy as np
 infilename1 = "result average.csv"
 infilename2 = "fake_result average.csv"
 
+dir1 = './result'
+dir2 = './fake_result'
+
+number_of_cars = 100
+#試行回数
+number_of_trials = 100
+
 duration_list1 = []
 distance_list1 = []
-infile1 = open(infilename1,"r",encoding="utf-8")
+file_path1 = os.path.join(dir1,infilename1)
+infile1 = open(file_path1,"r",encoding="utf-8")
 #対応車両台数
 half_number_of_cars = 50
 line_counter = 0
@@ -39,7 +48,8 @@ for i in range(half_number_of_cars):
 
 duration_list2 = []
 distance_list2 = []
-infile2 = open(infilename2,"r",encoding="utf-8")
+file_path2 = os.path.join(dir2,infilename2)
+infile2 = open(file_path2,"r",encoding="utf-8")
 line_counter = 0
 for line in infile2:
     """if line_counter == 0:
@@ -66,20 +76,30 @@ for i in range(half_number_of_cars):
 
 # duration's U
 #移動時間の転倒数
-U = 0
-for j in range(len(duration_list_sorted2)):
-    for k in range(len(duration_list_sorted1)):
-        if duration_list_sorted2[j] > duration_list_sorted1[k]:
-            U += 1
+U_avg = 0
+U_sum = 0
+for i in range(number_of_trials):
+    U = 0
+    for j in range(len(duration_list_sorted2)):
+        for k in range(len(duration_list_sorted1)):
+            if duration_list_sorted2[j] > duration_list_sorted1[k]:
+                U += 1
+    U_sum += U
+U_avg = U_sum/number_of_trials
 
-print("duration:",U,"(average:",len(duration_list_sorted1)*len(duration_list_sorted2)/2,")")
+print("duration:",U_avg,"(average:",len(duration_list_sorted1)*len(duration_list_sorted2)/2,")")
 
 # distance's U
 #移動距離の転倒数
-U = 0
-for j in range(len(distance_list_sorted2)):
-    for k in range(len(distance_list_sorted1)):
-        if distance_list_sorted2[j] > distance_list_sorted1[k]:
-            U += 1
+U_avg = 0
+U_sum = 0
+for i in range(number_of_trials):
+    U = 0
+    for j in range(len(distance_list_sorted2)):
+        for k in range(len(distance_list_sorted1)):
+            if distance_list_sorted2[j] > distance_list_sorted1[k]:
+                U += 1
+    U_sum += U
+U_avg = U_sum/number_of_trials
 
-print("distance:",U,"(average:",len(distance_list_sorted1)*len(distance_list_sorted2)/2,")")
+print("distance:",U_avg,"(average:",len(distance_list_sorted1)*len(distance_list_sorted2)/2,")")
