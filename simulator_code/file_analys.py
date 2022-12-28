2c4e86aa805a81f3b36f263f235835f9aa1219fd
Arimochi <112501001+Arimochi@users.noreply.github.com>
2022-12-28 23:33:16 +0900

    Add files via upload

--- simulator_code/file_analys.py
+++ simulator_code/file_analys.py
@@ -67,9 +67,9 @@ for file_name in os.listdir(dir):
 
 
 for i in range(number_of_cars):
-    duration_avg_list.append(int(duration_list[i]/(counter-1)))
+    duration_avg_list.append(int(duration_list[i]/(counter)))
 for i in range(number_of_cars):
-    distance_avg_list.append(round(distance_list[i]/(counter-1),1))
+    distance_avg_list.append(round(distance_list[i]/(counter),1))
     
 #print("移動時間")
 #print(duration_list)
