1a1059d93887d2b2193e291a1a7cb7a4bf83fa0b
Arimochi <112501001+Arimochi@users.noreply.github.com>
2022-12-29 03:40:34 +0900

    Add files via upload

--- simulator_code/file_analys.py
+++ simulator_code/file_analys.py
@@ -7,22 +7,22 @@ import matplotlib.pyplot as plt
 
 #変更箇所
 number_of_cars = 300
-#dir = './result'
-dir = './fake_result'
-#output_name = "result average.csv"
-output_name = "fake_result average.csv"
-#folder_name = "result"
-folder_name = "fake_result"
+dir = './result'
+#dir = './fake_result'
+output_name = "result average.csv"
+#output_name = "fake_result average.csv"
+folder_name = "result"
+#folder_name = "fake_result"
 
 #count_file = 0
 counter = 0
 
-duration_list = []
 duration_all_list = []
 duration_avg_list = []
-distance_list = []
 distance_all_list = []
 distance_avg_list = []
+duration_std = []
+distance_std = []
 
 for file_name in os.listdir(dir):
     file_path = os.path.join(dir,file_name)
@@ -40,8 +40,45 @@ for file_name in os.listdir(dir):
         distance_list1.append(float(data_list[1]))
         line_counter += 1
     infile1.close()
+
+    duration_all_list.append(duration_list1)
+    distance_all_list.append(distance_list1)
+
+#平均の計算
+duration_avg_list = np.mean(duration_all_list, axis=0)
+distance_avg_list = np.mean(distance_all_list, axis=0)
+#標準偏差の計算
+duration_std = np.std(duration_all_list, axis=0)
+distance_std = np.std(distance_all_list, axis=0)
+
+
+#print(duration_avg_list)
+#print(distance_avg_list)
+#print(duration_std)
+#print(distance_std)
+
+plt.hist(distance_avg_list, bins=50, rwidth=0.9, color='b')
+plt.savefig(folder_name  + '/' + "総移動距離の平均.png")
+plt.clf()
+
+plt.hist(distance_std, bins=50, rwidth=0.9, color='b')
+plt.savefig(folder_name  + '/' + "総移動距離の標準偏差.png")
+plt.clf()
+
+plt.hist(duration_avg_list, bins=50, rwidth=0.9, color='b')
+plt.savefig(folder_name  + '/' + "ゴールタイムの平均.png")
+plt.clf()
+
+plt.hist(distance_std, bins=50, rwidth=0.9, color='b')
+plt.savefig(folder_name  + '/' + "ゴールタイムの標準偏差.png")
+plt.clf()
+
+"""with open(folder_name  + '/' + output_name, 'w', newline='') as f:
+    writer = csv.writer(f)
+    for i in range(len(duration_avg_list)):
+        writer.writerow([duration_avg_list[i],distance_avg_list[i]])"""
     
-    if counter == 0:
+"""if counter == 0:
         for i in range(number_of_cars):
             duration_list.append(duration_list1[i])
         for i in range(number_of_cars):
@@ -67,13 +104,13 @@ for file_name in os.listdir(dir):
 
 
 for i in range(number_of_cars):
-    duration_avg_list.append(int(duration_list[i]/(counter)))
+    duration_avg_list.append(int(duration_list[i]/counter))
 for i in range(number_of_cars):
-    distance_avg_list.append(round(distance_list[i]/(counter),1))
+    distance_avg_list.append(round(distance_list[i]/counter,1))
     
 #print("移動時間")
-#print(duration_list)
-#print(duration_avg_list)
+print(duration_list)
+print(duration_avg_list)
 #print("移動距離")
 #print(distance_list)
 #print(distance_avg_list)
@@ -88,4 +125,4 @@ plt.clf()
 with open(folder_name  + '/' + output_name, 'w', newline='') as f:
     writer = csv.writer(f)
     for i in range(len(duration_avg_list)):
-        writer.writerow([duration_avg_list[i],distance_avg_list[i]])
+        writer.writerow([duration_avg_list[i],distance_avg_list[i]])"""
